f6e52d17122ff3395c4b448a74f0a4cb59793ec9
Lavanya <lavanyaj0725@gmail.com>
2024-01-12 23:39:11 +0530

    Add

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
-import React from 'react';
-import Navbar from './components/Navbar'; 
-import Home from "./components/Home";
-import Properties from "./components/Properties";
-import Client from "./components/Client";
-import Footer from "./components/Footer";
+import Navbar from './components/Navbar/Navbar'; 
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Journey from './components/Journey/Journey';
+import Footer from "./components/Footer/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Navbar />
-      <Home />
-      <Properties />
-      <Client />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Journey />
       <Footer />
+      </BrowserRouter>
     </div>
   );
 }
